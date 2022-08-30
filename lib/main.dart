38ae5d4cf1b1507af2fc6226f9a016da8a518175
yazanR2000 <yazanradaideh123@gmail.com>
2022-08-30 14:18:52 +0300

    Assignment 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,90 +1,134 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:myapp/dummy_data.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner:false,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text("Assgnmet 2"),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  print("Assignment 2");
-                },
-                icon: const Icon(Icons.person),
-              ),
-            ],
+        child: Birthday(),
+      ),
+    );
+  }
+}
+
+class Birthday extends StatefulWidget {
+  const Birthday({Key? key}) : super(key: key);
+
+  @override
+  State<Birthday> createState() => _BirthdayState();
+}
+
+class _BirthdayState extends State<Birthday> {
+  Color _color = Colors.red;
+  int _index = 0;
+  final Map _yazanInformation = Dummy_Data().YazanInfo;
+
+  final List<String> _qoutes = Dummy_Data().Qoutes;
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Assgnmet 2"),
+        backgroundColor: Colors.red,
+        actions: [
+          IconButton(
+            onPressed: () {
+              print(_yazanInformation);
+            },
+            icon: const Icon(Icons.info),
           ),
-          drawer: Drawer(
-            child: Column(
-              children: [
-                ListTile(
-                  onTap: (){},
-                  title: const Text("Home"),
-                  leading: const Icon(Icons.home),
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            ListTile(
+              onTap: () {},
+              title: const Text("Home"),
+              leading: const Icon(Icons.home),
+            ),
+            ListTile(
+              onTap: () {},
+              title: const Text("My repositories"),
+              leading: const Icon(Icons.category),
+            ),
+            ListTile(
+              onTap: () {},
+              title: const Text("LTUC profile"),
+              leading: const Icon(Icons.person_rounded),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.all(5),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 1),
+            borderRadius: BorderRadius.circular(20),
+            color: _color,
+          ),
+          //Swidth: 200,
+          duration: const Duration(milliseconds: 1000),
+          curve: Curves.linear,
+          child: Card(
+            elevation: 20,
+            shadowColor: Colors.red.shade200,
+            color: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: AnimatedContainer(
+              duration: const Duration(microseconds: 800),
+              curve: Curves.easeIn,
+              padding: const EdgeInsets.all(50),
+              
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: _color,
+              ),
+              //margin: const EdgeInsets.all(15),
+              child: Text(
+                _qoutes[_index],
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
                 ),
-                ListTile(
-                  onTap: (){},
-                  title: const Text("My repositories"),
-                  leading: const Icon(Icons.category),
-                ),
-                ListTile(
-                  onTap: (){},
-                  title: const Text("LTUC profile"),
-                  leading: const Icon(Icons.person_rounded),
-                ),
-              ],
+              ),
             ),
           ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-              
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.black, width: 1),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.purple,
-                      spreadRadius: 1,
-                      blurRadius: 10,
-                      offset: Offset(3, 5),
-                    ),
-                    BoxShadow(
-                      color: Colors.black38,
-                      spreadRadius: 1,
-                      blurRadius: 15,
-                      offset: Offset(0, 2),
-                    ),
-                  ],
-                  color: Colors.blueAccent,
-                ),
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(20),
-                margin: const EdgeInsets.symmetric(horizontal: 40),
-                child: const Text(
-                  "hello i am inside a container",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                  ),
-                ),
-              ),
-            ],
-          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final random = Random();
+            _color = Color.fromRGBO(
+              random.nextInt(256),
+              random.nextInt(256),
+              random.nextInt(256),
+              1,
+            );
+            _index = random.nextInt(11);
+          });
+        },
+        child: const Icon(Icons.change_circle),
       ),
     );
   }
